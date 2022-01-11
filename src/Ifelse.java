5d5200f78ae98a5c5c52f91759079f21445cfc9f
anil kumar <anilgulla913@gmail.com>
2022-01-11 20:23:02 +0530

    fifth commit

---
+++ src/Ifelse.java
@@ -0,0 +1,28 @@
+
+public class Ifelse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int testscore = 44;
+		char grade;
+		
+		if (testscore >=90) {
+		grade = 'A';
+     System.out.println("Grade :"+grade);
+	}else if (testscore>= 80){
+		grade ='B';
+		System.out.println("Grade:"+grade);
+	}else if (testscore>=70) {
+		grade = 'C';
+		System.out.println("Grade:"+grade);
+	
+	}else if (testscore>=60) {
+		grade = 'D';
+		System.out.println("Grade:"+grade);
+	}else   {
+		grade = 'F';
+		System.out.println("Grade:"+grade);
+	}
+}
+}
---
+++ src/Nestedif.java
@@ -0,0 +1,28 @@
+
+public class Nestedif {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i =50;
+		
+		if(i==50) {
+			System.out.println("i value is 50");
+			if(i<55) {
+				System.out.println("i is Smaller than 75");
+				if(i<55) {
+					System.out.println("i is smaller than 55");
+				}
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
