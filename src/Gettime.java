751c6463aa9ba663944ae4b639243ee0dceaecb5
Anil kumar <anilgulla913@gmail.com>
2022-01-25 11:19:55 +0530

    java commit

---
+++ src/Gettime.java
@@ -0,0 +1,14 @@
+import java.time.LocalDateTime;
+
+public class Gettime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LocalDateTime dt = LocalDateTime.now();
+		
+		System.out.println(dt.toString());
+		
+	}
+
+}
